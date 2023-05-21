c422bc71a8e8bcb469fbb10fdaee8624103a2ccc
kamosah <68175563+kamosah@users.noreply.github.com>
2023-05-21 10:57:41 -0400

    feat(PRIM-5): Add react query

--- apps/primo/src/app/app.tsx
+++ apps/primo/src/app/app.tsx
@@ -1,8 +1,17 @@
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from '@tanstack/router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { router } from '../components/Routes';
 
+const queryClient = new QueryClient();
+
 export function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
