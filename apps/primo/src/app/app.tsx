01fc0bbacec37c91e4b4d74ee1451bf0d960a2ec
kamosah <68175563+kamosah@users.noreply.github.com>
2023-05-21 22:05:13 -0400

    chore(PRIM-6): Remove @tanstack/router

--- apps/primo/src/app/app.tsx
+++ apps/primo/src/app/app.tsx
@@ -1,14 +1,13 @@
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { RouterProvider } from '@tanstack/router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { router } from '../components/Routes';
+import NxWelcome from './nx-welcome';
 
 const queryClient = new QueryClient();
 
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <NxWelcome title="Welcome Primo" />
       <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
     </QueryClientProvider>
   );
