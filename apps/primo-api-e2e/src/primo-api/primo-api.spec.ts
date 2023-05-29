8b59d3e9d35e7cc3a35a00f7af6cb39e25e02b87
kamosah <68175563+kamosah@users.noreply.github.com>
2023-05-29 15:25:11 -0400

    chore(PRIM-17): Fixing api-e2e test

--- apps/primo-api-e2e/src/primo-api/primo-api.spec.ts
+++ apps/primo-api-e2e/src/primo-api/primo-api.spec.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
+import { HOST, PORT, API_URL } from '@primo-api/api-interface';
 
 describe('GET /', () => {
   it('should return a message', async () => {
-    const res = await axios.get(`/`);
+    const res = await axios.get(`${HOST}:${PORT}${API_URL}`);
 
     expect(res.status).toBe(200);
-    expect(res.data).toEqual({ message: 'Hello API' });
+    expect(res.data).toEqual({ message: 'Welcome to primo-api!' });
   });
 });
--- libs/api-interface/src/lib/api-interface.ts
+++ libs/api-interface/src/lib/api-interface.ts
@@ -1,3 +1,7 @@
 export function apiInterface(): string {
   return 'api-interface';
 }
+
+export const API_URL = '/api';
+export const HOST = 'http://localhost';
+export const PORT = '3333';
